adb855e76fc1350475214397ee5d6d0679da9d11
Ray Dunning <raydunning@Rays-Air.lan>
2020-07-22 13:40:28 -0500

    added an about me page

---
+++ src/components/Content.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+
+
+function Content (props){
+
+    return(
+        <Container fluid={true}>
+            <Row className="justify-content-center">
+                <Col md={8}> 
+                {props.children} 
+                </Col>
+            </Row>
+        </Container>
+    )
+
+}
+
+
+
+export default Content 
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,22 @@
 import React from 'react';
-
+import Content from '../components/Content'
+import Hero from '../components/Hero'
 
 function AboutPage(props) {
     
     return(
-       <div>AboutPage</div>
+     <div>
+         <Hero title={props.title} />
+
+         <Content>
+         <p>I am a software engineer and Army veteran with a passion for technology and just creating things.</p>
+         <p> I've gone so far from where I was just a few years ago, it really shows how hard work pays off.</p> 
+         <p> I love creating with teams on new and exciting projects.</p>
+         <p> I am always up to challenge myself to tackle tough problems and learn and grow from it. </p>
+         <p> I'm a fun person and love to have a few laughs while enjoying the work I'm doing. </p>
+         </Content>
+
+     </div>
     )
 }
 

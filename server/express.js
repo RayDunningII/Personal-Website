95060c458d865a40bb1a9bba69c3ed35798616ef
Ray Dunning <raydunning@Rays-Air.lan>
2020-07-29 13:42:03 -0500

    make it better

--- server/express.js
+++ server/express.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 4000;
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const sgMail = require('@sendgrid/mail');

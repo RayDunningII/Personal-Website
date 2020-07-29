f70080067ef6b5cb397d3226ef4039075ec34ce5
Ray Dunning <raydunning@Rays-Air.lan>
2020-07-29 14:31:12 -0500

    make it better

--- server/express.js
+++ server/express.js
@@ -23,17 +23,13 @@ app.use((req, res, next) => {
     next();
 });
 
-   if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('server/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'server', 'build', 'index.html'));
-  });
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('build'));
 }
-
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
 
 app.get('/api', (req, res, next) => {
     res.send('API Status: Up and Running!')

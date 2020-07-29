60a2433127b69c3da9c24f71dd8ccc1ddecfe65c
Ray Dunning <raydunning@Rays-Air.lan>
2020-07-29 14:15:01 -0500

    make it better

---
+++ build/precache-manifest.ff9011ce7cd80aaef40b440eab2f3fc7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "781731312c48cc7685340ab91cda34ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8d324cc5d70164a6769",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "bb7cfdca1cadeaec9e49",
+    "url": "/static/css/main.21040ab1.chunk.css"
+  },
+  {
+    "revision": "d8d324cc5d70164a6769",
+    "url": "/static/js/2.a8ccaafb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a8ccaafb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb7cfdca1cadeaec9e49",
+    "url": "/static/js/main.55fc826a.chunk.js"
+  },
+  {
+    "revision": "4e298d7ae7623fec8b7e",
+    "url": "/static/js/runtime-main.b85d870a.js"
+  },
+  {
+    "revision": "c4f8de5e45dbcc43df46d6696390d002",
+    "url": "/static/media/github.c4f8de5e.png"
+  },
+  {
+    "revision": "483729835d7cdb0daf28ca16c12c2243",
+    "url": "/static/media/linkedin.48372983.jpg"
+  },
+  {
+    "revision": "f9b974df072296d5bf04067672edc971",
+    "url": "/static/media/lvlup.f9b974df.png"
+  }
+]);

9ceea603038a87ee6cf22582c29a094b68fe6bb0
Ray Dunning <raydunning@Rays-Air.lan>
2021-01-03 13:17:13 -0600

    make it better

--- server/express.js
+++ server/express.js
@@ -28,7 +28,7 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
     // add other server routes to path array
-    app.use(proxy(['/api' ], { target: 'http://localhost:3000' }));
+    app.use(proxy(['/api' ], { target: 'http://localhost:4000' }));
 } 
 
 if(process.env.NODE_ENV === 'production'){

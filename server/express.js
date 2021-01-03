7ee4fd79706607395dc26c1703ebe00133444c31
Ray Dunning <raydunning@Rays-Air.lan>
2021-01-03 13:22:13 -0600

    make it better

--- server/express.js
+++ server/express.js
@@ -14,6 +14,7 @@ const app = express();
 
 app.use(bodyParser.json());
 // app.use(express.static(path.resolve(__dirname,'public')))
+app.use(express.static(__dirname + '/'));
 app.use(cors());
 app.listen(port);
 
